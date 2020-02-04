33fc6d79a31517de60b0896042fb433af5ef8a97
Mitul118 <59639271+Mitul118@users.noreply.github.com>
2020-02-04 01:45:08 -0800

    Add files via upload
    Define function

---
+++ add define.py
@@ -0,0 +1,20 @@
+x=int(input("Enter value of x:"))
+y=int(input("Enter the value of y:"))
+
+def add(x,y):
+    z=x+y
+    print(z)
+
+def sub(x,y):
+    z=x-y
+    print(z)
+
+def mul(x,y):
+    z=x*y
+    print(z)
+
+def div(x,y):
+    z=x//y
+    print(z)
+
+
